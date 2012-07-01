1ad2586cd2f40768d61f956600b324e9f45276f5
ectech <jamil@evanschambers.com>
2012-07-01 02:25:51 -0400

    attempt to get a hold of the graphDb spring bean in the app context.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,6 +6,7 @@ class BootStrap {
     def destroy = {
 		println "Goodnight!"
 		// shut down the graph database - initated in resources.groovy
-		graphDb.shutdown();
+		def ctx = org.codehaus.groovy.grails.web.context.ServletContextHolder.servletContext.getAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.APPLICATION_CONTEXT);
+		ctx.graphDb.shutdown();
     }
 }

3428e5d8927e357f3221ff620ee6a2a60bc6af02
jamil <jamil@evanschambers.com>
2012-10-25 21:27:41 -0400

    Default war file name change

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.war.destFile = "Bookbook.war"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
